174a451f9aa100b6f5c7b3892a460ce92fb00ec1
Lovepreet Singh <118345008+Lovepreet04@users.noreply.github.com>
2023-07-04 11:34:12 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+var typed2 = new Typed('#CWL', {
+  strings: ['CodeWithLovepreet'],
+  typeSpeed: 80,
+});
+var typed = new Typed('#element', {
+  strings: ['C Programming', 'Python', 'C++', 'DSA', 'Java', 'Machine Learning', 'Data Science', 'Web Development'],
+  typeSpeed: 100,
+});
+
+
+
+
---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+var typed3 = new Typed('#my-name', {
+    strings: ['Lovepreet Singh'],
+    typeSpeed: 100,
+});
+
+var typed = new Typed('#element', {
+    strings: ['FrontEnd Developer', 'Web Developer', 'BackEnd Developer'],
+    typeSpeed: 100,
+});
